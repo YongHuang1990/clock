a9226afb205e5dd8122e143254e007c75bf09ccc
YongHuang1990 <zondar42@hotmail.com>
2018-11-14 16:54:54 +0800

    first commit

---
+++ Clock/FormClock.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Clock
+{
+    partial class FormClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("PMingLiU", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelTime.Location = new System.Drawing.Point(53, 60);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(132, 48);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "label1";
+            // 
+            // timerClock
+            // 
+            this.timerClock.Interval = 1000;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // FormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 294);
+            this.Controls.Add(this.labelTime);
+            this.Name = "FormClock";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timerClock;
+    }
+}
+
---
+++ Clock/FormClock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class FormClock : Form
+    {
+        public FormClock()
+        {
+            InitializeComponent();
+
+            timerClock.Enabled = true;
+            getTime();
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            getTime();
+        }
+
+        public void getTime()
+        {
+            DateTime time = DateTime.Now;
+            labelTime.Text = time.Year+"/"+time.Month+"/"+time.Day+"\n\n"+time.Hour + ":" + time.Minute + ":" + time.Second;
+        }
+    }
+}
